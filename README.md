5b406ff22f3260c7c36d661312b85bf316a38308
sefidel <contact@sefidel.net>
2023-04-15 16:49:25 +0900

    project: initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+.github
+=======
+
+Nix project template
---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Project template";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ ];
+
+        pkgs = import nixpkgs { inherit system overlays; };
+      in
+      rec {
+        # packages.tira = example;
+        # defaultPackage = example;
+
+        # apps.example = flake-utils.lib.mkApp {
+        #   drv = packages.example;
+        # };
+
+
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+          ];
+          buildInputs = with pkgs; [
+          ] ++ pkgs.lib.optionals (pkgs.stdenv.isDarwin) (with pkgs.darwin.apple_sdk.frameworks; [
+          ]);
+        };
+      });
+}
